6ca03544ee1e25fd63d0faf3f23155ce295014ee
ishalohia <ishalohia478@gmail.com>
2023-04-16 20:56:45 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Homepage from "./Pages/Homepage";
+import { Route, Routes } from "react-router-dom";
+import Chatpage from "./Pages/Chatpage";
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route path="/" element={<Homepage/> }> </Route>
+      <Route path="/chats" element={<Chatpage/> }> </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
